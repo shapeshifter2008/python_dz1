b52780e009a81527c69599f3301f150614c99575
Nikita Kurdin <shapeshifter2008@yandex.ru>
2022-03-26 23:19:24 +0300

    Урок 4. ДЗ 4-5. Импорт из своего модуля

---
+++ units.py
@@ -0,0 +1,17 @@
+import requests
+import xml.etree.ElementTree as ET
+from datetime import datetime
+
+def currency_rates(currency):
+    currency = currency.upper()
+    url_data = 'http://www.cbr.ru/scripts/XML_daily.asp'
+    response = requests.get(str(url_data)).text
+    xml_parse = ET.fromstring(response)
+
+    date_doc = datetime.strptime(xml_parse.attrib['Date'], '%d.%m.%Y')
+
+    for char_code in xml_parse.findall('Valute'):
+        if (char_code.find('CharCode').text == currency):
+            return f"{float(char_code.find('Value').text.replace(',', '.'))} {date_doc}"
+
+    return None
