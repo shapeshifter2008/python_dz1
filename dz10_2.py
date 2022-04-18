163d097cf22dd88b68e471957ef1f5863b7bbc25
Nikita Kurdin <shapeshifter2008@yandex.ru>
2022-04-18 17:58:12 +0300

    Урок 10. ДЗ 2. Суммарный расход ткани

---
+++ dz10_2.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class Clothes(ABC):
+    def __init__(self, param):
+        self.param = param
+
+    @abstractmethod
+    def calculate(self):
+        pass
+
+class Coat(Clothes):
+    @property
+    def calculate(self):
+        return round((self.param / 6.5) + 0.5)
+
+class Suit(Clothes):
+    @property
+    def calculate(self):
+        return round((2 * self.param) + 0.3)
+
+coat = Coat(48)
+suit = Suit(178)
+
+print(coat.calculate)
+print(suit.calculate)
